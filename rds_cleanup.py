1f8e6cddb3bddc7acb254c22da30f147d41a12c4
ben parafina <ben@djed.com>
2015-07-06 15:55:43 -0500

    starting in on the snapshot logic and date invalidation

--- rds_cleanup.py
+++ rds_cleanup.py
@@ -3,16 +3,14 @@ import pprint
 from boto import rds2
 import boto
 
-## for future use tomodularize script
+DAYS_EXPIRE = 7
+
+## for future use toi modularize script
 region = os.getenv('region')
 conn = boto.rds2.connect_to_region('us-east-1',aws_access_key_id=os.getenv('aws_access_key_id'),aws_secret_access_key=os.getenv('aws_secret_access_key'))
 
-def touch (fname, times=None):
-    with file(fname, 'a'):
-        os.utime(fname, times)
-
-
 def GetDBset():
+    ## generate the dbset
     dbs = []
     conn.describe_db_instances()
     instances = instances['DescribeDBInstanceResponse']['DescribeDBInstanceResult']['DBInstances']
@@ -23,12 +21,20 @@ def GetDBset():
 
 
 def Getsnapshots():
-    conn.describe_db_instances()
-    instances = conn.describe_db_instances()
+    ## get snapshot list to clean up
     snapshots = conn.describe_db_snapshots()
     list_snapshots = snapshots['DescribeDBSnapshotsRespone']['DescribeDBSnapshotsResult']['DBSnapshots']
+    for f in list_snapshots:
+        snaptype = f['SnapshotType']
+        if snaptype == 'manual':
+            continue
+        snap_id = f['DBSnapshotIdentifier']
+        delta_date = datetime.timedelta(days=DAYS_EXPIRE)
+        acceptable_date = datetime.datetime.now() - delta_date
+
 
 def Snapshotcleanup():
+    ## delete those snaps
     try:
         foo = bar
     except:
