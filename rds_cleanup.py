a5c549f6b7cd881b458a036b0b1b6118b5834fb1
ben parafina <ben@djed.com>
2015-07-07 11:38:38 -0500

    had to remove some crappy files, but now handles snapshots with manually generated names

--- rds_cleanup.py
+++ rds_cleanup.py
@@ -2,49 +2,58 @@ import os, re, time
 import pprint
 from boto import rds2
 import boto
+import datetime
 
-DAYS_EXPIRE = 7
-
-## for future use toi modularize script
+DAYS_EXPIRE = 4
+## for future use to modularize script
 region = os.getenv('region')
 conn = boto.rds2.connect_to_region('us-east-1',aws_access_key_id=os.getenv('aws_access_key_id'),aws_secret_access_key=os.getenv('aws_secret_access_key'))
 
 def GetDBset():
     ## generate the dbset
     dbs = []
-    conn.describe_db_instances()
-    instances = instances['DescribeDBInstanceResponse']['DescribeDBInstanceResult']['DBInstances']
+    instances = conn.describe_db_instances()
+    instances = instances['DescribeDBInstancesResponse']['DescribeDBInstancesResult']['DBInstances']
     for db in instances:
         dbname = db['DBInstanceIdentifier']
         dbs.append(dbname)
-    return(dbs)
+    db_list = dbs
 
 
 def Getsnapshots():
     ## get snapshot list to clean up
+    dead_snaps = []
     snapshots = conn.describe_db_snapshots()
-    list_snapshots = snapshots['DescribeDBSnapshotsRespone']['DescribeDBSnapshotsResult']['DBSnapshots']
+    list_snapshots = snapshots['DescribeDBSnapshotsResponse']['DescribeDBSnapshotsResult']['DBSnapshots']
+    pprint.pprint(list_snapshots)
     for f in list_snapshots:
         snaptype = f['SnapshotType']
         if snaptype == 'manual':
+            snap_name = f['DBSnapshotIdentifier']
+            print snap_name
+            snap_date = f['DBSnapshotIdentifier'][:-13]
+            delta_date = datetime.timedelta(days=DAYS_EXPIRE)
+            acceptable_date = datetime.datetime.now() - delta_date
+            datestr = re.search(r'\d{4}-\d{2}-\d{2}', snap_date).group()
+            try:
+                date_object = datetime.datetime.strptime(datestr, '%Y-%m-%d')
+            except:
+                pass
+            if (date_object < acceptable_date):
+                dead_snaps.append(snap_name)
             continue
-        snap_id = f['DBSnapshotIdentifier']
-        delta_date = datetime.timedelta(days=DAYS_EXPIRE)
-        acceptable_date = datetime.datetime.now() - delta_date
+    print dead_snaps
+    return dead_snaps
 
 
 def Snapshotcleanup():
     ## delete those snaps
-    try:
-        foo = bar
-    except:
-        pass
+    dead_snaps = Getsnapshots()
+    for f in dead_snaps:
+        print 'Removing ' + f + ' from RDS'
+        conn.delete_db_snapshot(f)
 
-if name == '__main__':
-    dir = 'var/lib/jenkins/jobs/prod-backup-list/rds-snapshots/'
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-
-    touch()
+if __name__ == '__main__':
+    GetDBset()
     Getsnapshots()
     Snapshotcleanup()
